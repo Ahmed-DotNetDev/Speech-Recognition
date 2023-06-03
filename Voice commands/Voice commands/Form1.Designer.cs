3defb9752cc2b218d59b1c36cbd80c537ffae903
Ahmed.net <Ahmed.net@gmail.com>
2023-06-04 00:31:24 +0300

    Adding project files

---
+++ Voice commands/Voice commands/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Voice_commands
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txt = new System.Windows.Forms.RichTextBox();
+			this.btntalk = new System.Windows.Forms.Button();
+			this.btnpause = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txt
+			// 
+			this.txt.Location = new System.Drawing.Point(46, 30);
+			this.txt.Name = "txt";
+			this.txt.Size = new System.Drawing.Size(248, 42);
+			this.txt.TabIndex = 0;
+			this.txt.Text = "";
+			// 
+			// btntalk
+			// 
+			this.btntalk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btntalk.Location = new System.Drawing.Point(179, 92);
+			this.btntalk.Name = "btntalk";
+			this.btntalk.Size = new System.Drawing.Size(115, 32);
+			this.btntalk.TabIndex = 1;
+			this.btntalk.Text = "Talk";
+			this.btntalk.UseVisualStyleBackColor = true;
+			this.btntalk.Click += new System.EventHandler(this.btntalk_Click);
+			// 
+			// btnpause
+			// 
+			this.btnpause.Enabled = false;
+			this.btnpause.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnpause.Location = new System.Drawing.Point(46, 92);
+			this.btnpause.Name = "btnpause";
+			this.btnpause.Size = new System.Drawing.Size(114, 32);
+			this.btnpause.TabIndex = 2;
+			this.btnpause.Text = "Pause Talk";
+			this.btnpause.UseVisualStyleBackColor = true;
+			this.btnpause.Click += new System.EventHandler(this.btnpause_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.ClientSize = new System.Drawing.Size(345, 166);
+			this.Controls.Add(this.btnpause);
+			this.Controls.Add(this.btntalk);
+			this.Controls.Add(this.txt);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "SPEECH RECOGNITION";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox txt;
+		private System.Windows.Forms.Button btntalk;
+		private System.Windows.Forms.Button btnpause;
+	}
+}
+
---
+++ Voice commands/Voice commands/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+namespace Voice_commands
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Choices voices = new Choices();
+			voices.Add(new string[] { "red", "blue", "white", "green", "yellow", "close", "stop","maximize","minimize" , "give me time now" });
+			GrammarBuilder grammarBuilder = new GrammarBuilder();
+			grammarBuilder.Append(voices);
+			Grammar grammar = new Grammar(grammarBuilder);
+			sre.LoadGrammarAsync(grammar);
+			sre.SetInputToDefaultAudioDevice();
+			sre.SpeechRecognized += SpeechRec;
+		}
+
+		private void SpeechRec(object sender, SpeechRecognizedEventArgs e)
+		{
+			switch (e.Result.Text)
+			{
+				case "red":this.BackColor = Color.Red;
+					txt.Text = "The Color Is Red Now !";
+					break;
+				case "blue":
+					this.BackColor = Color.Blue;
+					txt.Text = "The Color Is Blue Now !";
+					break;
+				case "white":
+					this.BackColor = Color.White;
+					txt.Text = "The Color Is White Now !";
+					break;
+				case "green":
+					this.BackColor = Color.Green;
+					txt.Text = "The Color Is Green Now !";
+					break;
+				case "yellow":
+					this.BackColor = Color.Yellow;
+					txt.Text = "The Color Is Yellow Now !";
+					break;
+				case "close":
+					this.Close();
+					break;
+				case "stop":
+					service();
+					break;
+				case "maximize":
+					this.WindowState=FormWindowState.Maximized;
+					txt.Text = "The Color Is Maximized Now !";
+					break;
+				case "minimize":
+					this.WindowState=FormWindowState.Normal;
+					txt.Text = "The Color Is Minimized Now !";
+					break;
+				case "give me time now":
+					DateTime currentDateTime = DateTime.Now;
+					txt.Text =currentDateTime.ToString();
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		private void btntalk_Click(object sender, EventArgs e)
+		{
+			sre.RecognizeAsync(RecognizeMode.Multiple);
+			btntalk.Enabled = false;
+			btnpause.Enabled = true;
+		}
+		private void service()
+		{
+			sre.RecognizeAsyncStop();
+			btntalk.Enabled = true;
+			btnpause.Enabled = false;
+		}
+		private void btnpause_Click(object sender, EventArgs e)
+		{
+			service();
+		}
+	}
+}
